e46db930187bbde0a11cb3e656561cc614c7b5a2
urbannio <urbanniotsai@gmail.com>
2015-06-08 09:23:32 +0800

    v1

---
+++ CookieHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Net;
+namespace DMMPasser
+{
+    class CookieHandler
+    {
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool InternetSetCookie(string lpszUrlName, string lbszCookieName, string lpszCookieData);
+
+        public void PassDMM()
+        {
+            Cookie temp1 = new Cookie("ckcy", "1");
+            InternetSetCookie("http://www.dmm.com/netgame/", null, temp1.ToString() + "; expires = Sun, 01-Jan-2019 00:00:00 GMT");
+        }
+    }
+
+   
+}
---
+++ DMMPasser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Threading.Tasks;
+using Grabacr07.KanColleViewer.Composition;
+using Grabacr07.KanColleWrapper;
+
+namespace DMMPasser
+{
+    [Export(typeof(IToolPlugin))]
+    [ExportMetadata("Title", "DMMPasser")]
+    [ExportMetadata("Description", "Pass DMM by edit cookies")]
+    [ExportMetadata("Version", "1.0")]
+    [ExportMetadata("Author", "@Urbannio")]
+    public class DMMPasser : IToolPlugin
+    {
+       
+        public string ToolName
+        {
+            get { return "DMMPasser"; }
+        }
+
+        public object GetSettingsView()
+        {
+            return null;
+        }
+
+        public object GetToolView()
+        {
+            return new PasserView { };
+        }
+    }
+
+
+   
+}
